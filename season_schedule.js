2e9273772c692f9374073338204d4a5bc2b60a9e
Joseph Dale Christesen <joseph.dale.christesen@mpq.mpg.de>
2018-08-17 18:38:29 +0200

    fixed the promotion relegation problem

--- season_schedule.js
+++ season_schedule.js
@@ -14,7 +14,6 @@ $(document).ready(function(){
 function change_schedule_year(){
     var year = $('#schedule_year_dropdown').find(':selected').text();
     var prev_year = $('#schedule_year_dropdown').find(':selected').next().text();
-    console.log(prev_year)
     var months = {'January': '01', 'February': '02', 'March': '03', 'April': '04',
     'May': '05', 'June': '06', 'July': '07', 'August': '08', 'September': '09',
     'October': '10', 'November': '11', 'December': '12'};
@@ -24,16 +23,40 @@ function change_schedule_year(){
     });
 
     var matches = {};
-    var team_dates = {};
+    var team_badges = {};
 
     $.when(
         $.getJSON('results/premier_league_results_' + year + '.json'),
-        $.getJSON('results/premier_league_results_' + prev_year + '.json')
-    ).done(function(current_year, prev_year) {
-        current_year = current_year[0]
-        prev_year = prev_year[0]
-        for (i = 0; i < current_year.length; i++){
-            data_date = current_year[i]['date'].split(' ');
+        $.getJSON('results/premier_league_results_' + prev_year + '.json'),
+        $.getJSON('premier_league_tables.json')
+    ).done(function(current_schedule, prev_schedule, tables) {
+        var current_schedule = current_schedule[0];
+        var prev_schedule = prev_schedule[0];
+        var tables = tables[0];
+        var current_teams = [];
+        for (i = 0; i < tables[year].length; i++){
+            current_teams.push(tables[year][i]['badge']);
+        }
+
+        var prev_teams = [];
+        for (i = 0; i < tables[prev_year].length; i++){
+            var index = current_teams.indexOf(tables[prev_year][i]['badge']);
+            if (index != -1){
+                current_teams.splice(index, 1);
+            } else {
+                prev_teams.push(tables[prev_year][i]['badge']);
+            }
+        }
+        current_teams.sort()
+        prev_teams.sort()
+        var rel_teams = {};
+        for (i = 0; i < current_teams.length; i++){
+            rel_teams[prev_teams[i]] = current_teams[i]
+        }
+
+
+        for (i = 0; i < current_schedule.length; i++){
+            data_date = current_schedule[i]['date'].split(' ');
             if (data_date[1].length == 1){
                 data_date = data_date[3] + '/' + months[data_date[2]] + '/0' + data_date[1]
             }else {
@@ -42,15 +65,18 @@ function change_schedule_year(){
 
             date = '<td>' + data_date + '</td>';
 
-            home_team = current_year[i]['home_team']['team'];
-            away_team = current_year[i]['away_team']['team'];
+            home_team = current_schedule[i]['home_team']['team'];
+            away_team = current_schedule[i]['away_team']['team'];
 
             if (home_team == 'Spurs'){
                 team = '<td>' + away_team + '</td>';
-                result = current_year[i]['home_team']['result'];
+                badge = current_schedule[i]['away_team']['badge'].split(' ')[1]
+                result = current_schedule[i]['home_team']['result'];
             } else if (away_team == 'Spurs'){
                 team = '<td>' + home_team + ' (A) </td>';
-                result = current_year[i]['away_team']['result'];
+                badge = current_schedule[i]['home_team']['badge'].split(' ')[1];
+                badge += ' (A)'
+                result = current_schedule[i]['away_team']['result'];
             } else {
                 continue
             }
@@ -68,29 +94,42 @@ function change_schedule_year(){
                 result = ''
                 points = 0;
             }
-            if ('score' in current_year[i]){
-                score = '<td class="' + result + '">' + current_year[i]['score'] +  '</td>';
+            if ('score' in current_schedule[i]){
+                score = '<td class="' + result + '">' + current_schedule[i]['score'] +  '</td>';
             } else {
                 score = '<td></td>'
             }
 
-            team_dates[team] = data_date;
+            team_badges[badge] = data_date;
             matches[data_date] = {'team': team,
                                   'score': score,
-                                  'points': points};
-        }// end current_year for loop
+                                  'points': points,
+                                  'badge': badge};
+        }// end current_schedule for loop
 
-         for (i = 0; i < prev_year.length; i++){
+        for (i = 0; i < prev_schedule.length; i++){
 
-            home_team = prev_year[i]['home_team']['team'];
-            away_team = prev_year[i]['away_team']['team'];
+            home_team = prev_schedule[i]['home_team']['team'];
+            away_team = prev_schedule[i]['away_team']['team'];
+            home_badge = prev_schedule[i]['home_team']['badge'].split(' ')[1];
+            away_badge = prev_schedule[i]['away_team']['badge'].split(' ')[1];
 
             if (home_team == 'Spurs'){
                 team = '<td>' + away_team + '</td>';
-                result = prev_year[i]['home_team']['result'];
+                result = prev_schedule[i]['home_team']['result'];
+                if (away_badge in rel_teams){
+                    badge = rel_teams[away_badge] + ' (A)'
+                } else {
+                    badge = away_badge + ' (A)'
+                }
             } else if (away_team == 'Spurs'){
                 team = '<td>' + home_team + ' (A) </td>';
-                result = prev_year[i]['away_team']['result'];
+                result = prev_schedule[i]['away_team']['result'];
+                if (home_badge in rel_teams){
+                    badge = rel_teams[home_badge]
+                } else {
+                    badge = home_badge
+                }
             } else {
                 continue
             }
@@ -108,18 +147,17 @@ function change_schedule_year(){
                 result = ''
                 points = 0;
             }
-            if ('score' in prev_year[i]){
-                score = '<td class="' + result + '">' + prev_year[i]['score'] +  '</td>';
+            if ('score' in prev_schedule[i]){
+                score = '<td class="' + result + '">' + prev_schedule[i]['score'] +  '</td>';
             } else {
                 score = '<td></td>'
             }
 
-            if (team in team_dates){
-                console.log(team)
-                matches[team_dates[team]]['prev_score'] = score;
-                matches[team_dates[team]]['prev_points'] = points;
+            if (badge in team_badges){
+                matches[team_badges[badge]]['prev_score'] = score;
+                matches[team_badges[badge]]['prev_points'] = points;
             }
-        }// end prev_year for loop
+        }// end prev_schedule for loop
 
         var keys = Object.keys(matches);
         keys.sort()
@@ -127,7 +165,6 @@ function change_schedule_year(){
         var current_points = 0
         var prev_points = 0
         for (var i = 0; i < keys.length; i++){
-            console.log(keys[i]);
             var match = matches[keys[i]];
             new_row = '<tr><td></td>';
             new_row += match['team'];
